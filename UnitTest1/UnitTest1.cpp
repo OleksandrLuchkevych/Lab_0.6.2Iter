06675b3b5145ef12a3320685a47a99b6428d05d3
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2022-11-11 00:27:29 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_0.6.2Iter/Lab_0.6.2Iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[] = { 2, 6, 8, 21, 9 };
+			int S = Sum(a, 5);
+
+			Assert::AreEqual(S, 27);
+		}
+	};
+}
